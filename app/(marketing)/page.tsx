8406b84d1bcc042f4d3f7b7fe6a10cb035401e05
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-16 22:37:22 +0800

    Adding styles and buttons

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,12 +1,32 @@
+import Link from 'next/link'
+import localFont from 'next/font/local'
+import { Poppins } from 'next/font/google'
 import { Medal } from 'lucide-react'
+
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+
+// const headingFont = localFont({
+//   src: '../../public/fonts/font.woff2',
+// })
+
+const textFont = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+})
 
 const MarketingPage = () => {
   return (
     <div className="flex items-center justify-center flex-col">
-      <div className="flex item-center justify-center flex-col">
-        <div className="text-center mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
+      <div
+        className={cn(
+          'flex items-center justify-center flex-col'
+          // headingFont.classNam
+        )}
+      >
+        <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
           <Medal className="h-6 w-6 mr-2" />
-          No 1 task management
+          No 1 task managment
         </div>
         <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
           Taskify helps team move
@@ -15,11 +35,23 @@ const MarketingPage = () => {
           work forward.
         </div>
       </div>
-      <div className="">
+      <div
+        className={cn(
+          'text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto',
+          textFont.className
+        )}
+      >
         Collaborate, manage projects, and reach new productivity peaks. From
         high rises to the home office, the way your team works is unique -
         accomplish it all with Taskify.
       </div>
+      <Button
+        className="mt-6"
+        size="lg"
+        asChild
+      >
+        <Link href="/sign-up">Get Taskify for free</Link>
+      </Button>
     </div>
   )
 }
