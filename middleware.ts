9cd9e5e0331f2c906dbfeb62450add0eae7d07b4
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-23 16:00:12 +0800

    Adding ORG logics in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,36 @@
-import { authMiddleware } from '@clerk/nextjs'
+import { authMiddleware, redirectToSignIn } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   publicRoutes: ['/'],
+  // This is ORG redirect
+  afterAuth(auth, req) {
+    if (auth.userId && auth.isPublicRoute) {
+      let path = '/select-org'
+
+      if (auth.orgId) {
+        path = `/organization/${auth.orgId}`
+      }
+
+      const orgSelection = new URL(path, req.url)
+
+      return NextResponse.redirect(orgSelection)
+    }
+
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url })
+    }
+
+    // Force user to create an ORG otherwise won't be able to continue
+    // If logged in without an ORG, can't continue either.
+    if (auth.userId && !auth.orgId && req.nextUrl.pathname !== '/select-org') {
+      const orgSelection = new URL('/select-org', req.url)
+      return NextResponse.redirect(orgSelection)
+    }
+  },
 })
 
 export const config = {
