026263395f1d16d140d46a8de69e15f7de94ce57
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-23 21:50:19 +0800

    Update page.tsx

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,30 @@
+import { create } from '@/actions/create-board'
+
 const OrganizationIdPage = () => {
-  return <div className="">Organization Page</div>
+  // async function create(formData: FormData) {
+  //   'use server'
+
+  //   const title = formData.get('title') as string
+
+  //   await db.board.create({
+  //     data: {
+  //       title,
+  //     },
+  //   })
+  // }
+  return (
+    <div className="">
+      <form action={create}>
+        <input
+          id="title"
+          name="title"
+          required
+          placeholder="Enter a board title"
+          className="border-black border p-1"
+        />
+      </form>
+    </div>
+  )
 }
 
 export default OrganizationIdPage
