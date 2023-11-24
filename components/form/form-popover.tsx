49d963ec451a64b34a88e537aa4753953787dd1f
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-24 22:15:33 +0800

    Update form-popover.tsx

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -42,6 +42,7 @@ export const FormPopover = ({
 
   const onSubmit = (formData: FormData) => {
     const title = formData.get('title') as string
+    const image = formData.get('image') as string
 
     execute({ title })
   }
