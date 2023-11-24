4f7537eb38e2eca450e0fc4341d405a82304aab9
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-24 14:40:26 +0800

    Adding info

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
@@ -1,5 +1,24 @@
 'use client'
 
+import { useOrganization } from '@clerk/nextjs'
+import Image from 'next/image'
+
 export const Info = () => {
-  return <div className="">Info Page</div>
+  const { organization, isLoaded } = useOrganization()
+
+  if (!isLoaded) {
+    return <p>Loading...</p>
+  }
+  return (
+    <div className="flex items-center gap-x-4">
+      <div className="w-[60px] h-[60px] relative">
+        <Image
+          fill
+          src={organization?.imageUrl!}
+          alt="Organization"
+          className="rounded-md object-cover"
+        />
+      </div>
+    </div>
+  )
 }
