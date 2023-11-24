9e5d34a4d56cc11ce8c0e08c8c0d0d26324a9f86
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-24 16:09:57 +0800

    Adding popover close

--- app/(platform)/(dashboard)/_components/board-list.tsx
+++ app/(platform)/(dashboard)/_components/board-list.tsx
@@ -1,3 +1,4 @@
+import { FormPopover } from '@/components/form/form-popover'
 import { Hint } from '@/components/hint'
 import { HelpCircle, User2 } from 'lucide-react'
 
@@ -9,19 +10,24 @@ export const BoardList = () => {
         Your boards
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
-        <div
-          role="button"
-          className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
+        <FormPopover
+          sideOffset={10}
+          side="right"
         >
-          <p className="text-sm">Create new board</p>
-          <span className="text-xs">5 remaining</span>
-          <Hint
-            sideOffset={40}
-            description={`Free workspaces can have up to 5 open boards. For unlimited boards upgrade this workspace.`}
+          <div
+            role="button"
+            className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
           >
-            <HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]" />
-          </Hint>
-        </div>
+            <p className="text-sm">Create new board</p>
+            <span className="text-xs">5 remaining</span>
+            <Hint
+              sideOffset={40}
+              description={`Free workspaces can have up to 5 open boards. For unlimited boards upgrade this workspace.`}
+            >
+              <HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]" />
+            </Hint>
+          </div>
+        </FormPopover>
       </div>
     </div>
   )
