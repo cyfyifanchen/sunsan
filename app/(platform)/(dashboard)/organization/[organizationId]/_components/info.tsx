09c6eb4ea36fc2b36559eb04632e43fea97960a9
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-24 14:35:43 +0800

    Cleaning up forms
    now forms can be fund at components folder

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
@@ -0,0 +1,5 @@
+'use client'
+
+export const Info = () => {
+  return <div className="">Info Page</div>
+}
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,21 +1,9 @@
-import { db } from '@/lib/db'
-import { Board } from './board'
-import { Form } from './form'
+import { Info } from './_components/info'
 
 const OrganizationIdPage = async () => {
-  const boards = await db.board.findMany()
   return (
-    <div className="flex flex-col space-y-4">
-      <Form />
-      <div className="space-y-2">
-        {boards.map((board) => (
-          <Board
-            key={board.id}
-            title={board.title}
-            id={board.id}
-          />
-        ))}
-      </div>
+    <div className="w-full mb-20">
+      <Info />
     </div>
   )
 }
