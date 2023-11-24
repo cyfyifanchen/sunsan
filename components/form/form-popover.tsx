c4b50e54e1006ebc952f6c25241eceb252a7e4b4
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-24 17:15:51 +0800

    Adding validation to popover

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -13,6 +13,7 @@ import { FormInput } from './form-input'
 import { FormSubmit } from './form-submit'
 import { Button } from '@/components/ui/button'
 import { X } from 'lucide-react'
+import { toast } from 'sonner'
 
 interface FormPopoverProps {
   children: React.ReactNode
@@ -27,6 +28,22 @@ export const FormPopover = ({
   align,
   sideOffset = 0,
 }: FormPopoverProps) => {
+  const { execute, fieldErrors } = useAction(createBoard, {
+    onSuccess: (data) => {
+      console.log({ data })
+      toast.success('Board created!')
+    },
+    onError: (error) => {
+      console.log({ error })
+      toast.error(error)
+    },
+  })
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get('title') as string
+
+    execute({ title })
+  }
   return (
     <Popover>
       <PopoverTrigger asChild>{children}</PopoverTrigger>
@@ -47,12 +64,16 @@ export const FormPopover = ({
             <X className="h-4 w-4" />
           </Button>
         </PopoverClose>
-        <form className="space-y-4">
+        <form
+          action={onSubmit}
+          className="space-y-4"
+        >
           <div className="space-y-4">
             <FormInput
               id="title"
               label="Board title"
               type="text"
+              errors={fieldErrors}
             />
           </div>
           <FormSubmit className="w-full">Create</FormSubmit>
