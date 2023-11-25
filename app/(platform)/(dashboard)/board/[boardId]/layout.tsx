60f3292a57cf9f110ef96d3526b37531213a5172
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-25 23:06:45 +0800

    Adding layout page to individual board ID page.

---
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -0,0 +1,64 @@
+import { db } from '@/lib/db'
+import { auth } from '@clerk/nextjs'
+import { notFound, redirect } from 'next/navigation'
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { boardId: string }
+}) {
+  const { orgId } = auth()
+
+  if (!orgId) {
+    return {
+      title: 'Board',
+    }
+  }
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  })
+
+  return {
+    title: board?.title || 'Board',
+  }
+}
+
+const BoardIdLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode
+  params: { boardId: string }
+}) => {
+  const { orgId } = auth()
+
+  if (!orgId) {
+    redirect('/select-org')
+  }
+
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  })
+
+  if (!board) {
+    // Next14 feature
+    notFound()
+  }
+
+  return (
+    <div
+      className="relative h-full bg-no-repeat bg-cover bg-center"
+      style={{ backgroundImage: `url(${board.imageFullUrl})` }}
+    >
+      <main className="relative pt-28 h-full">{children}</main>
+    </div>
+  )
+}
+
+export default BoardIdLayout
