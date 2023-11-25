268618e80d393604d8106f4dbdcbcfb0d12a76f9
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-25 22:48:52 +0800

    Lodash to dynamic org title on the browser tab

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,5 +1,13 @@
+import { startCase } from 'lodash'
 import { OrgControl } from './_components/org-control'
+import { auth } from '@clerk/nextjs'
 
+export async function generateMetadata() {
+  const { orgSlug } = auth()
+  return {
+    title: startCase(orgSlug || 'organization'),
+  }
+}
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
