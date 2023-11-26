21a0c80e2a4c49ab37b2d138c6ea3737c6049762
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-26 20:29:27 +0800

    Adding using actions of creating list

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
@@ -8,8 +8,13 @@ import { FormInput } from '@/components/form/form-input'
 import { useParams } from 'next/navigation'
 import { FormSubmit } from '@/components/form/form-submit'
 import { Button } from '@/components/ui/button'
+import { useAction } from '@/hooks/use-action'
+import { createList } from '@/actions/create-list'
+import { toast } from 'sonner'
+import { useRouter } from 'next/navigation'
 
 export const ListForm = () => {
+  const router = useRouter()
   const params = useParams()
   const [isEditing, setIsEditing] = useState(false)
 
@@ -27,6 +32,17 @@ export const ListForm = () => {
     setIsEditing(false)
   }
 
+  const { execute, fieldErrors } = useAction(createList, {
+    onSuccess: (data) => {
+      toast.success(`List "${data.title}" created.`)
+      disableEditing()
+      router.refresh()
+    },
+    onError: (error) => {
+      toast.error(error)
+    },
+  })
+
   const onKeyDown = (e: KeyboardEvent) => {
     if (e.key === 'Escape') {
       disableEditing()
@@ -36,15 +52,27 @@ export const ListForm = () => {
   useEventListener('keydown', onKeyDown)
   useOnClickOutside(formRef, disableEditing)
 
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get('title') as string
+    const boardId = formData.get('boardId') as string
+
+    execute({
+      title,
+      boardId,
+    })
+  }
+
   if (isEditing) {
     return (
       <ListWrapper>
         <form
+          action={onSubmit}
           ref={formRef}
           className="w-full p-3 rounded-md bg-white space-y-4 shadow-md"
         >
           <FormInput
             ref={inputRef}
+            errors={fieldErrors}
             id="title"
             className="text-sm px-2 py-1 h-7 font-medium border-transparent hover:borer-input focus:border-input transition"
             placeholder="Enter list title..."
