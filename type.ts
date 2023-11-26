b561432dcae857a4f8debaca207b548a08d48b3b
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-26 15:43:19 +0800

    Creating type.ts in the root of the project to reuseabality

---
+++ type.ts
@@ -0,0 +1,6 @@
+// Creating types to be reused later
+
+import { Card, List } from '@prisma/client'
+
+export type ListWithCards = List & { cards: Card[] }
+export type CardWithList = Card & { list: List }
