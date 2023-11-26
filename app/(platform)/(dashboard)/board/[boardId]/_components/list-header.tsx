6270e5b5d792cd8573990bb187fa20346d79e1e6
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-26 21:06:50 +0800

    Adding list header and calling in from list item

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { List } from '@prisma/client'
+
+interface ListHeaderProps {
+  data: List
+}
+
+export const ListHeader = ({ data }: ListHeaderProps) => {
+  return (
+    <div className="pt-2 px-2 text-sm font-semibold flex justify-between items-start gap-x-2">
+      <div className="w-full text-sm px-2.5 py-1 h-7 font-medium border-transparent">
+        {data.title}
+      </div>
+    </div>
+  )
+}
--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { ListWithCards } from '@/type'
+import { ListHeader } from './list-header'
 
 interface ListItemProps {
   data: ListWithCards
@@ -9,7 +10,9 @@ interface ListItemProps {
 export const ListItem = ({ data, index }: ListItemProps) => {
   return (
     <li className="shrink-0 h-full w-[272px] select-none">
-      <div className="w-full rounded-md bg-[#1f1f2f4] shadow-md pb-2"></div>
+      <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
+        <ListHeader data={data} />
+      </div>
     </li>
   )
 }
