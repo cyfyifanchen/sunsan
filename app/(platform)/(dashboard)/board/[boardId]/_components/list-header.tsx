09921e6928a8e7e005c88188bbaeb3313fda28de
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-26 21:22:07 +0800

    Updating list header

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -1,17 +1,73 @@
 'use client'
 
+import { FormInput } from '@/components/form/form-input'
 import { List } from '@prisma/client'
-
+import { useState, useRef, ElementRef } from 'react'
+import { useEventListener } from 'usehooks-ts'
 interface ListHeaderProps {
   data: List
 }
 
 export const ListHeader = ({ data }: ListHeaderProps) => {
+  const [title, setTitle] = useState(data.title)
+  const [isEditing, setIsEditing] = useState(false)
+
+  const formRef = useRef<ElementRef<'form'>>(null)
+  const inputRef = useRef<ElementRef<'input'>>(null)
+
+  const enableEditing = () => {
+    setIsEditing(true)
+    setTimeout(() => {
+      inputRef.current?.focus()
+      inputRef.current?.select()
+    })
+  }
+
+  const disableEditing = () => {
+    setIsEditing(false)
+  }
+
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === 'Escape') {
+      formRef.current?.requestSubmit()
+    }
+  }
+
+  useEventListener('keydown', onKeyDown)
+
   return (
     <div className="pt-2 px-2 text-sm font-semibold flex justify-between items-start gap-x-2">
-      <div className="w-full text-sm px-2.5 py-1 h-7 font-medium border-transparent">
-        {data.title}
-      </div>
+      {isEditing ? (
+        <form className="flex-1 px-[2px]">
+          <input
+            hidden
+            id="id"
+            name="id"
+            value={data.id}
+          />
+          <input
+            hidden
+            id="boardId"
+            name="boardId"
+            value={data.boardId}
+          />
+          <FormInput
+            ref={inputRef}
+            onBlur={() => {}}
+            id="title"
+            placeholder="Enter list title..."
+            defaultValue={title}
+            className="text-sm px-[7px] py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition truncate bg-transparent focus:bg-white"
+          />
+        </form>
+      ) : (
+        <div
+          onClick={enableEditing}
+          className="w-full text-sm px-2.5 py-1 h-7 font-medium border-transparent"
+        >
+          {title}
+        </div>
+      )}
     </div>
   )
 }
