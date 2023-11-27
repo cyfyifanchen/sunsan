e23c3ab25949461d4a39c7effaa3db477cc6e3c2
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-27 20:10:26 +0800

    Updating the header and schema

--- actions/update-card/schema.ts
+++ actions/update-card/schema.ts
@@ -17,7 +17,7 @@ export const UpdateCard = z.object({
       required_error: 'Title is required',
       invalid_type_error: 'Title is required',
     })
-    .min(3, {
+    .min(2, {
       message: 'Title is too short',
     }),
   id: z.string(),
--- components/modals/card-modal/header.tsx
+++ components/modals/card-modal/header.tsx
@@ -62,8 +62,7 @@ export const Header = ({ data }: HeaderProps) => {
             onBlur={onBlur}
             id="title"
             defaultValue={title}
-            className="font-semibold text-xl px-1 text-neutral-700 bg-transparent border-transparent relative -left-1.5 w-[95%]
-            focus-visible:bg-white focus-visible:border-input mb-0.5 truncate"
+            className="font-semibold text-xl px-1 text-neutral-700 bg-transparent border-transparent relative -left-1.5 w-[95%] focus-visible:bg-white focus-visible:border-input mb-0.5 truncate"
           />
         </form>
         <p className="text-sm text-muted-foreground">
