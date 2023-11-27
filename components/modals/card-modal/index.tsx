0d86e156597cb41d1d2c56bfded64fc2faed946a
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-27 20:59:36 +0800

    Adding description

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -6,6 +6,7 @@ import { fetcher } from '@/lib/fetcher'
 import { CardWithList } from '@/type'
 import { useQuery } from '@tanstack/react-query'
 import { Header } from './header'
+import { Description } from './description'
 
 export const CardModal = () => {
   const id = useCardModal((state) => state.id)
@@ -24,6 +25,17 @@ export const CardModal = () => {
     >
       <DialogContent>
         {!cardData ? <Header.Skeleton /> : <Header data={cardData} />}
+        <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
+          <div className="col-span-3">
+            <div className="w-full space-y-6">
+              {!cardData ? (
+                <Description.Skeleton />
+              ) : (
+                <Description data={cardData} />
+              )}
+            </div>
+          </div>
+        </div>
       </DialogContent>
     </Dialog>
   )
