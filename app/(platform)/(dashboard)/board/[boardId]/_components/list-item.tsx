563818a721abcb385530f51bd974707b4a8af932
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-27 13:36:17 +0800

    Adding card items and import it in list items.
    Now ready for the drag and drop feature

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -5,6 +5,7 @@ import { ListHeader } from './list-header'
 import { ElementRef, useRef, useState } from 'react'
 import { CardForm } from './card-form'
 import { cn } from '@/lib/utils'
+import { CardItem } from './card-item'
 
 interface ListItemProps {
   data: ListWithCards
