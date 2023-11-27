fbbea2d3b7cd93684e3ac90b21d1f2725e8ec785
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-27 14:48:24 +0800

    Update list order is working with server action

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -6,6 +6,9 @@ import { useEffect, useState } from 'react'
 import { ListItem } from './list-item'
 import { DragDropContext, Droppable } from '@hello-pangea/dnd'
 import { LampDesk } from 'lucide-react'
+import { useAction } from '@/hooks/use-action'
+import { updateListOrder } from '@/actions/update-list-order'
+import { toast } from 'sonner'
 
 interface ListContainerProps {
   data: ListWithCards[]
@@ -22,6 +25,15 @@ function reorder<T>(list: T[], startIndex: number, endIndex: number) {
 
 export const ListContainer = ({ data, boardId }: ListContainerProps) => {
   const [orderedData, setOrderedData] = useState(data)
+
+  const { execute: executeUpdateListOrder } = useAction(updateListOrder, {
+    onSuccess: () => {
+      toast.success('List reordered')
+    },
+    onError: (error) => {
+      toast.error(error)
+    },
+  })
 
   useEffect(() => {
     setOrderedData(data)
@@ -47,6 +59,7 @@ export const ListContainer = ({ data, boardId }: ListContainerProps) => {
       )
 
       setOrderedData(items)
+      executeUpdateListOrder({ items, boardId })
     }
 
     // If move a card
