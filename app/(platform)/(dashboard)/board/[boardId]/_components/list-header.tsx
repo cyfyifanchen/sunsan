dcfc38de7b72959e57e710f92fa75d2501529de1
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-27 10:04:57 +0800

    Adding add card option to the card form

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -12,9 +12,10 @@ import { ListOptions } from './list-options'
 
 interface ListHeaderProps {
   data: List
+  onAddCard: () => void
 }
 
-export const ListHeader = ({ data }: ListHeaderProps) => {
+export const ListHeader = ({ data, onAddCard }: ListHeaderProps) => {
   const [title, setTitle] = useState(data.title)
   const [isEditing, setIsEditing] = useState(false)
 
@@ -115,7 +116,7 @@ export const ListHeader = ({ data }: ListHeaderProps) => {
       )}
       <ListOptions
         data={data}
-        onAddCard={() => {}}
+        onAddCard={onAddCard}
       ></ListOptions>
     </div>
   )
--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -2,16 +2,39 @@
 
 import { ListWithCards } from '@/type'
 import { ListHeader } from './list-header'
+import { ElementRef, useRef, useState } from 'react'
+import { CardForm } from './card-form'
 
 interface ListItemProps {
   data: ListWithCards
   index: number
 }
 export const ListItem = ({ data, index }: ListItemProps) => {
+  const textareaRef = useRef<ElementRef<'textarea'>>(null)
+  const [isEditing, setIsEditing] = useState(false)
+  const disableEditing = () => {
+    setIsEditing(false)
+  }
+  const enableEditing = () => {
+    setIsEditing(true)
+    setTimeout(() => {
+      textareaRef.current?.focus()
+    })
+  }
   return (
     <li className="shrink-0 h-full w-[272px] select-none">
       <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
-        <ListHeader data={data} />
+        <ListHeader
+          onAddCard={enableEditing}
+          data={data}
+        />
+        <CardForm
+          ref={textareaRef}
+          isEditing={isEditing}
+          enableEditing={enableEditing}
+          disableEditing={disableEditing}
+          listId={data.id}
+        />
       </div>
     </li>
   )
