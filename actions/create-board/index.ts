d1db776ede45ba24fdab3731010010c82f8251d1
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-29 22:16:41 +0800

    Adding org limit to board list

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -8,6 +8,7 @@ import { revalidatePath } from 'next/cache'
 import { createSafeAction } from '@/lib/create-safe-action'
 import { createAuditLog } from '@/lib/create-audit-log'
 import { ACTION, ENTITY_TYPE } from '@prisma/client'
+import { hasAvailableCount, incrementAvailableCount } from '@/lib/org-limit'
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth()
@@ -15,6 +16,14 @@ const handler = async (data: InputType): Promise<ReturnType> => {
   if (!userId || !orgId) {
     return {
       error: 'Unauthorized',
+    }
+  }
+
+  const canCreate = await hasAvailableCount()
+
+  if (!canCreate) {
+    return {
+      error: 'You have reached your limits of free boards.',
     }
   }
 
@@ -56,6 +65,8 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         imageUserName,
       },
     })
+
+    await incrementAvailableCount()
 
     await createAuditLog({
       entityId: board.id,
--- app/(platform)/(dashboard)/_components/board-list.tsx
+++ app/(platform)/(dashboard)/_components/board-list.tsx
@@ -1,7 +1,9 @@
 import { FormPopover } from '@/components/form/form-popover'
 import { Hint } from '@/components/hint'
 import { Skeleton } from '@/components/ui/skeleton'
+import { MAX_FREE_BOARDS } from '@/constants/boards'
 import { db } from '@/lib/db'
+import { getAvailableCount } from '@/lib/org-limit'
 import { auth } from '@clerk/nextjs'
 import { HelpCircle, User2 } from 'lucide-react'
 import Link from 'next/link'
@@ -22,6 +24,8 @@ export const BoardList = async () => {
       createdAt: 'desc',
     },
   })
+
+  const availableCount = await getAvailableCount()
 
   return (
     <div className="space-y-4">
@@ -50,7 +54,9 @@ export const BoardList = async () => {
             className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
           >
             <p className="text-sm">Create new board</p>
-            <span className="text-xs">5 remaining</span>
+            <span className="text-xs">{`${
+              MAX_FREE_BOARDS - availableCount
+            } remaining`}</span>
             <Hint
               sideOffset={40}
               description={`Free workspaces can have up to 5 open boards. For unlimited boards upgrade this workspace.`}
