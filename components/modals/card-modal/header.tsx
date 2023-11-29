69b6c20763d47cceac5f9beed08b9a888ba692bf
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-29 09:33:48 +0800

    Revalidating queries in header to reflect on the activities

--- components/modals/card-modal/header.tsx
+++ components/modals/card-modal/header.tsx
@@ -24,6 +24,10 @@ export const Header = ({ data }: HeaderProps) => {
         queryKey: ['card', data.id],
       })
 
+      queryClient.invalidateQueries({
+        queryKey: ['card-logs', data.id],
+      })
+
       toast.success(`Renamed to "${data.title}"`)
       setTitle(data.title)
     },
